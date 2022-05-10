c9f89e41cd4497eb0448b2b50ca23f664bac7e1d
Berk Bozkurt <BBozkurt@lcs.on.ca>
2022-05-10 09:02:54 -0400

    Initial Commit

---
+++ TheOfficeWikiApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TheOfficeWikiApp
+//
+//  Created by Berk Bozkurt on 2022-05-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TheOfficeWikiApp/TheOfficeWikiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheOfficeWikiAppApp.swift
+//  TheOfficeWikiApp
+//
+//  Created by Berk Bozkurt on 2022-05-10.
+//
+
+import SwiftUI
+
+@main
+struct TheOfficeWikiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
