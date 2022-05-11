d54b7a4f6dd51aa37cf0be6982ee9773e58da363
Berk Bozkurt <BBozkurt@lcs.on.ca>
2022-05-11 11:40:20 -0400

    Model has been created with the information we need. id, title, description

---
+++ TheOfficeWikiApp/Model/Episode.swift
@@ -0,0 +1,18 @@
+//
+//  Episodes.swift
+//  TheOfficeWikiApp
+//
+//  Created by Berk Bozkurt on 2022-05-11.
+//
+
+import Foundation
+
+
+struct Episode: Decodable {
+    
+    let episodeId: String
+    let episodeTitle: String
+    let episodeDescribtion: String
+    
+}
+
---
+++ TheOfficeWikiApp/Model/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  TheOfficeWikiApp
+//
+//  Created by Berk Bozkurt on 2022-05-11.
+//
+
+import Foundation
+
+struct SearchResult: Decodable {
+    
+    let results: [Episode]
+    
+}
