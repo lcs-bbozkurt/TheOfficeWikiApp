ce2150d2aa708b61e278cee5ab305b51ffe303b6
Berk Bozkurt <BBozkurt@lcs.on.ca>
2022-05-12 11:37:06 -0400

    Extra bracket has been removed from the preview.

--- TheOfficeWikiApp/Views/Helpers/FavouritesButtonView.swift
+++ TheOfficeWikiApp/Views/Helpers/FavouritesButtonView.swift
@@ -29,7 +29,7 @@ struct FavouritesButtonView: View {
 
 struct FavouritesButtonView_Previews: PreviewProvider {
     static var previews: some View {
-        FavouritesButtonView((episode: testEpisode,
+        FavouritesButtonView(episode: testEpisode,
                               inFavourites: .constant(true),
                               favourites: .constant([]))
     }
