17a2e504a47beb5f46de8eace4e5cc808ae9da11
Berk Bozkurt <BBozkurt@lcs.on.ca>
2022-05-12 11:34:53 -0400

    Started on making the button for implementing favourites.

---
+++ TheOfficeWikiApp/Views/Helpers/FavouritesButtonView.swift
@@ -0,0 +1,34 @@
+//
+//  FavouritesButtonView.swift
+//  TheOfficeWikiApp
+//
+//  Created by Berk Bozkurt on 2022-05-12.
+//
+
+import SwiftUI
+
+struct FavouritesButtonView: View {
+    
+    // MARK: Stored properties
+    
+    // The episode being shown
+    let episode: Episode
+    
+    // Is this song in the list of favourites?
+    @Binding var inFavourites: Bool
+    
+    // The list of favourite songs
+    @Binding var favourites: [Episode]
+        
+    // MARK: Computed properties
+        
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FavouritesButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        FavouritesButtonView()
+    }
+}
